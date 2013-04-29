a2e8c7cdccc161a245c25de44fc65547b8f10b3e
Walter Medvedeo <wmedvedeo@gmail.com>
2013-04-29 16:09:28 +0200

    annotations refactoring

--- jbpm-data-modeler-core/src/main/java/org/jbpm/datamodeler/annotations/Description.java
+++ jbpm-data-modeler-core/src/main/java/org/jbpm/datamodeler/annotations/Description.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target({ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface DataObject {
+public @interface Description {
+
+    String value() default "";
+
 }
--- jbpm-data-modeler-core/src/main/java/org/jbpm/datamodeler/annotations/Label.java
+++ jbpm-data-modeler-core/src/main/java/org/jbpm/datamodeler/annotations/Label.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target({ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Required {
+public @interface Label {
+
+    String value() default "";
+
 }
