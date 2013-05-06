93abc75b1e93b5a331380683c8d419e74e904f02
Jan Schatteman <jschatte@redhat.com>
2013-05-06 12:43:52 +0200

    Intermediate commit

--- jbpm-data-modeler-editor/jbpm-data-modeler-editor-client/src/main/java/org/jbpm/datamodeler/editor/client/editors/DataModelEditorPresenter.java
+++ jbpm-data-modeler-editor/jbpm-data-modeler-editor-client/src/main/java/org/jbpm/datamodeler/editor/client/editors/DataModelEditorPresenter.java
@@ -20,7 +20,6 @@ import org.jboss.errai.bus.client.api.RemoteCallback;
 import org.jboss.errai.ioc.client.api.Caller;
 import org.jbpm.datamodeler.editor.client.editors.resources.i18n.Constants;
 import org.jbpm.datamodeler.editor.client.validation.ValidatorService;
-import org.jbpm.datamodeler.editor.client.type.DataModelResourceType;
 import org.jbpm.datamodeler.editor.events.DataModelerEvent;
 import org.jbpm.datamodeler.editor.events.DataObjectSelectedEvent;
 import org.jbpm.datamodeler.editor.model.DataModelTO;
@@ -29,8 +28,10 @@ import org.jbpm.datamodeler.editor.service.DataModelerService;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.client.annotations.*;
 import org.uberfire.client.common.BusyPopup;
+import org.uberfire.client.context.WorkbenchContext;
 import org.uberfire.client.mvp.UberView;
 import org.uberfire.client.workbench.widgets.events.NotificationEvent;
+import org.uberfire.client.workbench.widgets.events.PathChangeEvent;
 import org.uberfire.client.workbench.widgets.menu.MenuFactory;
 import org.uberfire.client.workbench.widgets.menu.MenuItem;
 import org.uberfire.client.workbench.widgets.menu.Menus;
@@ -39,19 +40,18 @@ import org.uberfire.client.workbench.widgets.toolbar.ToolBar;
 import org.uberfire.client.workbench.widgets.toolbar.ToolBarItem;
 import org.uberfire.client.workbench.widgets.toolbar.impl.DefaultToolBar;
 import org.uberfire.client.workbench.widgets.toolbar.impl.DefaultToolBarItem;
-import org.uberfire.shared.mvp.PlaceRequest;
 
-import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.uberfire.client.workbench.widgets.menu.MenuFactory.newSimpleItem;
 
-
-@Dependent
-@WorkbenchEditor(identifier = "DataModelEditor", supportedTypes = {DataModelResourceType.class })
+//@Dependent
+//@WorkbenchEditor(identifier = "DataModelEditor", supportedTypes = {DataModelResourceType.class })
+@WorkbenchScreen(identifier = "dataModelerScreen")
 public class DataModelEditorPresenter {
 
     public interface DataModelEditorView
@@ -69,6 +69,9 @@ public class DataModelEditorPresenter {
     private DataModelEditorView view;
 
     @Inject
+    private NewDataObjectPopup newDataObjectPopup;
+
+    @Inject
     private Caller<DataModelerService> modelerService;
 
     @Inject
@@ -79,9 +82,6 @@ public class DataModelEditorPresenter {
     private ToolBar toolBar;
 
     @Inject
-    private NewDataObjectPopup newDataObjectPopup;
-
-    @Inject
     private DataModelEditorSelectionModel selectionModel;
 
     @Inject
@@ -90,11 +90,14 @@ public class DataModelEditorPresenter {
     @Inject
     private Event<NotificationEvent> notification;
 
-    private Path path;
+    @Inject
+    private WorkbenchContext workbenchContext;
+
+    private Path currentProject;
 
     @WorkbenchPartTitle
     public String getTitle() {
-        return Constants.INSTANCE.modelEditor_screen_name() + " [" + (path != null ? path.getFileName() : "") + "]";
+        return Constants.INSTANCE.modelEditor_screen_name() + " [" + (currentProject != null ? currentProject.getFileName() : "") + "]";
     }
 
     @WorkbenchPartView
@@ -102,119 +105,57 @@ public class DataModelEditorPresenter {
         return view;
     }
 
-    public DataModelTO getDataModel() {
-        return selectionModel.getSelectedModel();
+    @OnStart
+    public void onStart() {
+        makeMenuBar();
+        makeToolBar();
+        processPathChange(workbenchContext.getActivePath());
     }
-
-    /*
-    TODO remover este metodo cuando ya no lo necesitemos
-    lo dejo por si necesitamos hacer copy paste de la parte de las valiaciones.
-
-    public Command createAddDataObjectCommand(final String packageName, final String name, final String superClassName) {
-        return new Command() {
-
-            //TODO validate that the package name is a valid java package name
-
-            @Override
-            public void execute() {
-                //TODO this method remains here by now until we decide if a data object can be created from different
-                //IU places. (toolbar, menu, and current button)
-
-                validatorService.isValidIdentifier(name, new ValidatorCallback() {
-                    @Override
-                    public void onFailure() {
-                        ErrorPopup.showMessage("Invalid data object identifier: " + name + " is not a valid Java identifier");
-                    }
-
-                    @Override
-                    public void onSuccess() {
-
-                        //TODO if whe have a valid packageName and a valid class name
-                        //we can proceed to uniqueEntiyName validation
-
-                        //TODO dejar esto prolijo
-                        validatorService.isUniqueEntityName(packageName, name, getDataModel(), new ValidatorCallback() {
-                            @Override
-                            public void onFailure() {
-                                ErrorPopup.showMessage("A data object with identifier: " + name + " already exists in the model.");
-                            }
-
-                            @Override
-                            public void onSuccess() {
-                                DataObjectTO dataObject = new DataObjectTO(name);
-                                dataObject.setPackageName(getDataModel().getDefaultPackage());
-                                getDataModel().getDataObjects().add(dataObject);
-
-                                //view.addDataObject(dataObject);
-                                selectionModel.setSelectedObject(dataObject);
-
-                                notification.fire(new NotificationEvent(Constants.INSTANCE.modelEditor_notification_dataObject_created(name)));
-                                dataModelerEventEvent.fire(new DataObjectCreatedEvent("", getDataModel(), dataObject));
-
-                            }
-                        });
-                    }
-                });
-            }
-        };
-    }
-    */
 
     @IsDirty
     public boolean isDirty() {
-        //Window.alert("isDirty invoked");
+        //TODO implement dirty status management
         return false;
-    }
-
-    @OnClose
-    public void onClose() {
-        //Window.alert("onClose invoked");
-    }
-
-    @OnFocus
-    public void onFocus() {
-        //Window.alert("onFocus invoked");
     }
 
     @OnMayClose
     public boolean onMayClose() {
-        //Window.alert("onMayClose");
+        //TODO implement dirty status management
         return true;
     }
 
     @OnSave
     public void onSave() {
+
+        BusyPopup.showMessage("Saving project datamodel");
         modelerService.call(new RemoteCallback<Object>() {
                 @Override
                 public void callback(Object response) {
-                    //when the model is saved without errors
-                    //clean the deleted dataobjects status, mark all dataobjects as persisted, etc.
+                    BusyPopup.close();
                     restoreModelStatus();
                     notification.fire(new NotificationEvent(Constants.INSTANCE.modelEditor_notification_dataModel_saved()));
                 }
             },
             new DataModelerErrorCallback("An error was produced during data model saving.")
-        ).saveModel(getDataModel(), path);
+        ).saveModel(getDataModel(), currentProject);
     }
 
-    @OnStart
-    public void onStart(Path path, PlaceRequest placeRequest) {
-
-        //The onStart method must read the file and load the DataModel to be edited.
-        makeMenuBar();
-        makeToolBar();
-
-        this.path = path;
-        loadContent(path);
+    @WorkbenchMenu
+    public Menus getMenus() {
+        return menus;
     }
 
-    private void loadContent(final Path path) {
+    @WorkbenchToolBar
+    public ToolBar getToolBar() {
+        return this.toolBar;
+    }
+
+    private void loadProjectDataModel(final Path path) {
 
         BusyPopup.showMessage(Constants.INSTANCE.modelEditor_loading());
 
         modelerService.call(
                 new RemoteCallback<List<PropertyTypeTO>>() {
-
                     @Override
                     public void callback(List<PropertyTypeTO> baseTypes) {
                         view.setBaseTypes(baseTypes);
@@ -236,6 +177,12 @@ public class DataModelEditorPresenter {
                 },
                 new DataModelerErrorCallback("An error was produced when the requested data model was loaded from server.")
         ).loadModel(path);
+
+        currentProject = path;
+    }
+
+    public DataModelTO getDataModel() {
+        return selectionModel.getSelectedModel();
     }
 
     private void setDataModel(DataModelTO dataModel) {
@@ -248,36 +195,58 @@ public class DataModelEditorPresenter {
         }
     }
 
-    @OnReveal
-    public void onReveal() {
-        //Window.alert("onReveal");
-    }
-
     private void onNewDataObject() {
         newDataObjectPopup.setDataModel(getDataModel());
         newDataObjectPopup.show();
     }
 
-    @WorkbenchMenu
-    public Menus getMenus() {
-        return menus;
+    private void onPathChange(@Observes final PathChangeEvent event) {
+        processPathChange(event.getPath());
+    }
+
+    private void processPathChange(Path newPath) {
+
+        if (newPath != null) {
+
+            modelerService.call(
+                    new RemoteCallback<Path>() {
+                        @Override
+                        public void callback(Path projectPath) {
+
+                            if (projectPath != null) {
+                                if (!projectPath.equals(currentProject)) {
+                                    loadProjectDataModel(projectPath);
+                                }
+                            } else {
+                                //TODO
+                                //check what will be the default policy when the user click in a place
+                                //that doesn't belong to a project.
+                            }
+                        }
+                    },
+                    new DataModelerErrorCallback("An error was produced during current project path calculation.")
+            ).resolveProject(newPath);
+
+        } else {
+            //TODO check if this is posible. By definition we will always have a path.
+        }
+    }
+
+    private void restoreModelStatus() {
+        //when the model is saved without errors
+        //clean the deleted dataobjects status, mark all dataobjects as persisted, etc.
+        getDataModel().setPersistedStatus();
     }
 
     private void makeMenuBar() {
         menus = MenuFactory
-                    .newTopLevelMenu(Constants.INSTANCE.modelEditor_menu_file())
+                    .newTopLevelMenu(Constants.INSTANCE.modelEditor_menu_main())
                     .withItems( getItems() )
                     .endMenu().build();
     }
 
-    @WorkbenchToolBar
-    public ToolBar getToolBar() {
-        return this.toolBar;
-    }
-
     private void makeToolBar() {
-        toolBar = new DefaultToolBar( "dataModeler" );
-
+        toolBar = new DefaultToolBar( "dataModelerToolbar" );
 
         org.uberfire.client.mvp.Command saveCommand = new org.uberfire.client.mvp.Command() {
             @Override
@@ -334,7 +303,60 @@ public class DataModelEditorPresenter {
         return menuItems;
     }
 
-    private void restoreModelStatus() {
-        getDataModel().setPersistedStatus();
+
+    /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+    /*
+    TODO remover este metodo cuando ya no lo necesitemos
+    lo dejo por si necesitamos hacer copy paste de la parte de las valiaciones.
+
+    public Command createAddDataObjectCommand(final String packageName, final String name, final String superClassName) {
+        return new Command() {
+
+            //TODO validate that the package name is a valid java package name
+
+            @Override
+            public void execute() {
+                //TODO this method remains here by now until we decide if a data object can be created from different
+                //IU places. (toolbar, menu, and current button)
+
+                validatorService.isValidIdentifier(name, new ValidatorCallback() {
+                    @Override
+                    public void onFailure() {
+                        ErrorPopup.showMessage("Invalid data object identifier: " + name + " is not a valid Java identifier");
+                    }
+
+                    @Override
+                    public void onSuccess() {
+
+                        //TODO if whe have a valid packageName and a valid class name
+                        //we can proceed to uniqueEntiyName validation
+
+                        //TODO dejar esto prolijo
+                        validatorService.isUniqueEntityName(packageName, name, getDataModel(), new ValidatorCallback() {
+                            @Override
+                            public void onFailure() {
+                                ErrorPopup.showMessage("A data object with identifier: " + name + " already exists in the model.");
+                            }
+
+                            @Override
+                            public void onSuccess() {
+                                DataObjectTO dataObject = new DataObjectTO(name);
+                                dataObject.setPackageName(getDataModel().getDefaultPackage());
+                                getDataModel().getDataObjects().add(dataObject);
+
+                                //view.addDataObject(dataObject);
+                                selectionModel.setSelectedObject(dataObject);
+
+                                notification.fire(new NotificationEvent(Constants.INSTANCE.modelEditor_notification_dataObject_created(name)));
+                                dataModelerEventEvent.fire(new DataObjectCreatedEvent("", getDataModel(), dataObject));
+
+                            }
+                        });
+                    }
+                });
+            }
+        };
     }
+    */
+
 }
